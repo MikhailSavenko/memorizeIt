41a2b180a54c8fbc0ebd1477d0bd0b7a1d609d11
Mike <misha47savenko@yandex.ru>
2025-08-28 18:33:44 +0300

    transcription_by_wordsapi None если нет транскрипции, дщля поля также разрешен null и default None

--- word/models.py
+++ word/models.py
@@ -16,7 +16,7 @@ PART_OF_SPEECH = [
 class Word(models.Model):
     word = models.CharField(max_length=255, verbose_name="English word", blank=False)
     part_of_speech = models.CharField(max_length=255, choices=PART_OF_SPEECH, verbose_name="Part of speech", default="another", blank=True)
-    transcription = models.CharField(max_length=255, verbose_name="Transcription", blank=True)
+    transcription = models.CharField(max_length=255, verbose_name="Transcription", blank=True, null=True)
     translation = models.CharField(max_length=255, verbose_name="Translation", blank=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
--- word/services.py
+++ word/services.py
@@ -10,7 +10,12 @@ logger = logging.getLogger(__name__)
 API_KEY = os.getenv("API_KEY")
 
 
-def transcription_by_wordsapi(word: str) -> dict:
+def transcription_by_wordsapi(word: str) -> dict|None:
+    """
+    Делает запрос к API wordsapiv1 для получения транскрипции слова и в случае успеха вернет словарь с транскрипцией.
+    В ином случае вернет None
+    pronontiation - Транскрипция из запрсоа.
+    """
     headers = {
         "x-rapidapi-key": API_KEY,
         "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
@@ -23,8 +28,7 @@ def transcription_by_wordsapi(word: str) -> dict:
         pronunciation = data.get("pronunciation", {"all": ""})["all"]
         return pronunciation
     except Exception as e:
-        logger.error(f"Ошибка при получении транскрипции {e}.")
-        pronunciation = ""
-        return pronunciation
+        logger.warning(f"Ошибка при получении транскрипции. Транскрипция не найдена: {e}.")
+        return None
         
 
