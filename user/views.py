12289629c18f1b809c2593f279ad13e4f524c552
Mikhail <misha47savenko@yandex.ru>
2026-04-26 15:19:25 +0300

    register html + login after register in view

--- user/views.py
+++ user/views.py
@@ -1,6 +1,11 @@
 from django.contrib.auth import get_user_model
 from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth import login
+from django.shortcuts import redirect
+
 from user.forms import RegisterUserForm
+
 
 User = get_user_model()
 
@@ -8,4 +13,11 @@ class RegisterUserView(CreateView):
     
     form_class = RegisterUserForm
     template_name = "user/register.html"
+    success_url = reverse_lazy("word:write_word")
 
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+    
