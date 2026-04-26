d440a652f4e18ea05e481bfd8b7f569112df0822
Mikhail <misha47savenko@yandex.ru>
2026-04-26 19:01:30 +0300

    register_form html

--- user/forms.py
+++ user/forms.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
@@ -9,3 +11,12 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email")
+    
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                "class": "form-control", 
+                "placeholder": field.label
+            })
