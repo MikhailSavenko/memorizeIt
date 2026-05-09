d823985a2d4ecb9494d35f52d096c812a4ca5e80
Mikhail <misha47savenko@yandex.ru>
2026-05-09 20:38:05 +0300

    LoginUserView

--- user/views.py
+++ user/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.views import LoginView
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth import login
 from django.shortcuts import redirect
 
-from user.forms import RegisterUserForm
+from user.forms import RegisterUserForm, AuthenticationUserForm
 
 
 User = get_user_model()
@@ -23,3 +24,10 @@ class RegisterUserView(CreateView):
         success_url = str(self.get_success_url())
         return redirect(success_url)
     
+
+class LoginUserView(LoginView):
+
+    form_class = AuthenticationUserForm
+    template_name = "user/login.html"
+
+
