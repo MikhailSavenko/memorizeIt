c0de29acf1f925133cc2db159e1f47348746837a
Mikhail <misha47savenko@yandex.ru>
2026-08-06 13:19:24 +0300

    widget=forms.widgets.SearchInput

--- word/forms.py
+++ word/forms.py
@@ -94,7 +94,7 @@ class SearchAliveForm(forms.Form):
 class SearchDictForm(forms.Form):
     search_dict = forms.CharField(max_length=50, 
                            required=False,
-                           widget=forms.SearchInput(
+                           widget=forms.widgets.SearchInput(
                                attrs={
                                    "placeholder": "Search by №, word and translation",
                                    "class": "form-control"
