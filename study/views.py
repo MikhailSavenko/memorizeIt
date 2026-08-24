6cf0323a1e7f9d760809f533861161364e53a09c
Mikhail <misha47savenko@yandex.ru>
2026-08-24 15:29:16 +0300

    import forms in views study

--- study/views.py
+++ study/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.views.generic import FormView
 from django.shortcuts import redirect
 
+from study.forms import ParametersCreateRoomForm, RepeatRoomForm
 from word.models import Word
 
 
